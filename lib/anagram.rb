0e36bcb5b60a8a94bf3fc7290178186e4312a522
Kristen <kriscadacio@gmail.com>
2022-04-05 14:56:55 -0400

    commit tree 55d34359b01d9e26f08f68a34fa43fd23923e0a1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+
+class Anagram
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (array)
+        array.filter {|word| word.split("").sort == @word.split("").sort }
+    end
+end
